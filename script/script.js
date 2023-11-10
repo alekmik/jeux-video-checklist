9b0f9eb7ee7aab8654dd802255834420f60425e0
Esoriak <somahaut.dev@gmail.com>
2023-11-10 09:31:34 +0100

    wip merge games component

--- script/script.js
+++ script/script.js
@@ -1,3 +1,13 @@
 function goToGame(url) {
     window.location.href = url;
 }
+
+function navigateToHome() {
+    window.location.href = url;
+}
+
+function toLogged() {
+    console.log("jsuis ici coucou");
+    window.location.href = url;
+}
+
