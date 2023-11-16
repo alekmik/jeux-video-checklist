e133379c20291c4273a456de7b544c5bd1302c32
Esoriak <somahaut.dev@gmail.com>
2023-11-16 16:05:43 +0100

    add functions for catch the name of user with the localstorage function

--- script/script.js
+++ script/script.js
@@ -2,12 +2,3 @@ function goToGame(url) {
     window.location.href = url;
 }
 
-function navigateToHome() {
-    window.location.href = url;
-}
-
-function toLogged() {
-    console.log("jsuis ici coucou");
-    window.location.href = url;
-}
-
